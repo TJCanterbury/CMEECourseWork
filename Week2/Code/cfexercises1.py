12ba41a4c4c8cf69db48eb4b10b69320dab70688
Tristan Canterbury <tnjncy@gmail.com>
2020-10-21 12:17:08 +0100

    On branch main  Your branch is up-to-date with 'origin/main'.  Changes to be committed: 	modified:   Week2/Code/cfexercises1.py 	new file:   Week3/Code/TreeHeight.R 	new file:   control_flow.R

--- Week2/Code/cfexercises1.py
+++ Week2/Code/cfexercises1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env ipython3
+#!/usr/bin/env python3
 
 """Some functions exemplifying the use of control statements"""
 __author__ = 'Tristan Canterbury (tjc19@ic.ac.uk)'
@@ -8,15 +8,18 @@ __version__ = '0.0.1'
 import sys
 
 ## functions ##
-def foo_1(x=25): # returns the square root of x
+def foo_1(x=25): 
+    """Returns the square root of x"""
     return x ** 0.5
 
-def foo_2(x=4, y=3): # returns the bigger of 2 values
+def foo_2(x=4, y=3): 
+    """returns the bigger of 2 values"""
     if x > y:
         return x
     return y
 
-def foo_3(x=0.023, y=0.99, z=0.001): # Moves the largest variable to the end of the vector
+def foo_3(x=0.023, y=0.99, z=0.001): 
+    """Moves the largest variable to the end of the vector"""
     if x > y:
         tmp = y
         y = x
@@ -27,18 +30,21 @@ def foo_3(x=0.023, y=0.99, z=0.001): # Moves the largest variable to the end of 
         y = tmp
     return [x, y, z]
 
-def foo_4(x=5): # Calculates factorial of x using the range function for explicitness
+def foo_4(x=5): 
+    """Calculates factorial of x using the range function for explicitness"""
     result = 1
     for i in range(1, x + 1):
         result = result * i
     return result
 
-def foo_5(x=6): # a recursive function that calculates the factorial of x
+def foo_5(x=6): 
+    """a recursive function that calculates the factorial of x"""
     if x == 1:
         return 1
     return x * foo_5(x - 1)
      
-def foo_6(x=7): # Calculate the factorial of x in a different way
+def foo_6(x=7): 
+    """Calculate the factorial of x in a different way"""
     facto = 1
     while x >= 1:
         facto = facto * x
@@ -46,12 +52,12 @@ def foo_6(x=7): # Calculate the factorial of x in a different way
     return facto
      
 def main(argv):
-    print(foo_1())
-    print(foo_2())
-    print(foo_3())
-    print(foo_4())
-    print(foo_5())
-    print(foo_6())
+    print(foo_1(10))
+    print(foo_2( 4, 7))
+    print(foo_3(3, 4, 1))
+    print(foo_4(4))
+    print(foo_5(7))
+    print(foo_6(8))
     return 0
 
 if (__name__ == "__main__"):
